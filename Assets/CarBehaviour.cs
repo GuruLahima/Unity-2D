f8c6cc4a1b576e102b2ccb9a0171272638602d56
Aleksandar Ruseski <name_not@live.com>
2023-05-11 19:54:35 +0200

    stuff

--- Assets/CarBehaviour.cs
+++ Assets/CarBehaviour.cs
@@ -8,12 +8,6 @@ public class CarBehaviour : MonoBehaviour
 
     [SerializeField] float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
