1a8694ef4fbaa9d654ce03ac30cf41e47078a91d
Aleksandar Ruseski <name_not@live.com>
2023-05-16 21:52:36 +0200

    class 10

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PauseMenu : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Should quit the game");
+#if UNITY_EDITOR
+        // works only in Unity Editor
+        EditorApplication.isPlaying = false;
+#else
+        // works only for builds
+        Application.Quit();
+#endif
+    }
+
+    public void PauseGame()
+    {
+        // pause physics and animatons that depend on timescale
+        Time.timeScale = 0f;
+    }
+
+    public void UnpauseGame()
+    {
+        // unpause game
+        Time.timeScale = 1;
+    }
+
+}
