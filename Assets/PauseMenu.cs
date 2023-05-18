ca76403f50601717cd7f65d03d17275ed843c65b
Aleksandar Ruseski <name_not@live.com>
2023-05-18 17:57:00 +0200

    preparation

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,34 +7,46 @@ public class PauseMenu : MonoBehaviour
 {
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    public void QuitGame()
-    {
-        Debug.Log("Should quit the game");
+  }
+
+  public void QuitGame()
+  {
+    Debug.Log("Should quit the game");
 #if UNITY_EDITOR
-        // works only in Unity Editor
-        EditorApplication.isPlaying = false;
+    // works only in Unity Editor
+    EditorApplication.isPlaying = false;
 #else
         // works only for builds
         Application.Quit();
 #endif
-    }
+  }
 
-    public void PauseGame()
-    {
-        // pause physics and animatons that depend on timescale
-        Time.timeScale = 0f;
-    }
+  public void PauseGame()
+  {
+    // pause physics and animatons that depend on timescale
+    Time.timeScale = 0f;
+  }
 
-    public void UnpauseGame()
-    {
-        // unpause game
-        Time.timeScale = 1;
-    }
+  public void UnpauseGame()
+  {
+    // unpause game
+    Time.timeScale = 1;
+  }
+
+  public void ToggleFullscreen(bool isFullscreen)
+  {
+    // Toggle fullscreen
+    Screen.fullScreen = isFullscreen;
+
+    EditorWindow window = EditorWindow.focusedWindow;
+    // Assume the game view is focused.
+    window.maximized = isFullscreen;
+
+    
+  }
 
 }
